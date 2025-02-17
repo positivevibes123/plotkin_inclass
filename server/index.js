09caa9e052bc8950e9430db77bd7caa926271f2b
Daniel Amoruso <103326339+positivevibes123@users.noreply.github.com>
2025-02-17 15:14:57 -0500

    In class 2/17/25
    Added work done in class (Went over node basics, npm, package.json)

---
+++ server/index.js
@@ -0,0 +1,24 @@
+/* Example comment */
+
+/* Load the http module to create an HTTP server. */
+const http = require("http");
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World\n");
+});
+
+/* The two functions below are identical:
+
+const square = x => x * x;
+
+function square2(x) {
+    return x * x;
+}*/
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+console.log("Hello World!");
