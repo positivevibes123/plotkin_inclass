81dabad00d7c5c85394aff8886ff5785ca64454c
Daniel Amoruso <103326339+positivevibes123@users.noreply.github.com>
2025-03-06 14:57:22 -0500

    Added unplug router (partially)
    Need to add the rest. Probably look at Plotkins github repo later

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,7 +1,19 @@
+<script>
+import useNow from '@vueuse/core'
+
+const {now, pause, resume} = useNow({controls: true})
+
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
     <h2>About the best class at New Paltz</h2>
+  </div>
+  <div>
+    <p>Current time: {{ now }}</p>
+    <button @click="pause">Pause</button>
+    <button @click="resume">Resume</button>
   </div>
 </template>
 
